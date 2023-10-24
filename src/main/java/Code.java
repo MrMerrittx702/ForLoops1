e24f25e0d9b05f1adca60f356bb14bb9e534dcff
Evan Merritt <109871289+MrMerrittx702@users.noreply.github.com>
2023-10-24 13:04:09 -0500

    Update and rename Code.java to ForLoops1.java

--- src/main/java/Code.java
+++
@@ -1,8 +0,0 @@
-
-public class Code {
-
-	public static void main(final String[] args) {
-		System.out.println("Helo world!");
-	}
-
-}
---
+++ src/main/java/ForLoops1.java
@@ -0,0 +1,67 @@
+
+public class ForLoops1 {
+
+  /**1) Implement the following method that counts up from 1 to n by 1s?
+   * Each value should be printed with a space after */
+  public static void countUp(int n) {
+ 
+  }
+  
+  /**2)Implement the following method that counts down from n to 1*/
+  public static void countDown(int n) {
+
+  }
+  
+  /**3)Implement the following method that counts up from 1 to n by the value x */
+  public static void countUpMultiples(int n, int x) {
+
+  }
+  
+  /**4)Implement the following method that counts down from n to 1 by the value x  */
+  public static void countDownMultiples(int n, int x) {
+
+  }
+  
+  /**5) Implement the following method using a for each loop that accepts a String[] array and prints out each element.*/
+  public static void enhancedFor(String[] array) {
+
+  }
+  
+  /**6) Implement the following method using a for loop that accepts a string str and prints out each character.
+   * Each character should be on the same line with a space in between.*/
+  public static void processString(String str) {
+
+  }
+  
+  /**7)Implement the following method using a for each loop that accepts a String[] array and prints out each element.
+    * Without using a for each loop. 
+    * Each element should be on the same line with a space in between.*/
+  public static void processArray(String[] array) {
+ 
+  }
+  
+  /**8) Implement the following method using a nested for loop
+   * Count from 1 to n, x number of times. 
+   * Each count, from 1 to n should be on the same line with space between each number.
+   * Each new count should be on a new line.*/
+  public static void nestedFor(int n, int x) {
+
+  }
+  
+  /**9) Implement the following method using a nested for loop
+   * Write a method to print out all values of a 2d array from beginning to end in a matrix
+   * A space seperating every element and a new line at the end of every row
+   * No for each loops
+   */
+  public static void process2dArray(String[][] array2d) {
+
+  }
+  
+  /**10) Implement the following method using a nested for loop
+   * Write a method to print out all values of a 2d array from beginning to end in a matrix
+   * A space seperating every element and a new line at the end of every row
+   */
+  public static void enhanced2dArray(String[][] array2d) {
+
+  } 
+}
