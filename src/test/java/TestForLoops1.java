e49ee29d35bcb1bd0ba33d27787db289f9e65d62
Evan Merritt <109871289+MrMerrittx702@users.noreply.github.com>
2023-10-24 13:13:35 -0500

    Update TestForLoops1.java

--- src/test/java/TestForLoops1.java
+++ src/test/java/TestForLoops1.java
@@ -1,13 +1,6 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;import org.junit.jupiter.api.DisplayName;import org.junit.jupiter.api.Test;import org.junit.jupiter.params.ParameterizedTest;import org.junit.jupiter.params.provider.CsvSource;import java.io.*;
 
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
-
-import java.io.*;
 
 
 
